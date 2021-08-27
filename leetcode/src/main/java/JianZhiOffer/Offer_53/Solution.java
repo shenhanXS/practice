e28a30a97bee076106001e7dca78c9b66418a52e
ShenHan <1264825375@qq.com>
2021-08-27 20:02:36 +0800

    日常做题
    剑指offer 53

---
+++ leetcode/src/main/java/JianZhiOffer/Offer_53/Solution.java
@@ -0,0 +1,28 @@
+package JianZhiOffer.Offer_53;
+
+/**
+ * @author Shenhan
+ * @create 2021-08-27-19:43
+ */
+public class Solution {
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[] nums = {1,2,3};
+        System.out.println(solution.missingNumber(nums));
+    }
+
+    // 二分法
+    public int missingNumber(int[] nums) {
+        int i = 0, j = nums.length - 1;
+        while (i <= j) {
+            int mid = (i + j) / 2;
+            if (mid == nums[mid]) {
+                i = mid + 1;
+            }else {
+                j = mid - 1;
+            }
+        }
+        return i;
+    }
+}
