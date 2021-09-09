d8be002209e6838d5ec58c7a98dc8f42489efab6
ShenHan <1264825375@qq.com>
2021-09-09 18:06:31 +0800

    日常提交
    494题

---
+++ leetcode/src/main/java/hot_100/test_494_findTargetSumWays/Solution_494.java
@@ -0,0 +1,35 @@
+package hot_100.test_494_findTargetSumWays;
+
+/**
+ * 给你一个整数数组 nums 和一个整数 target 。
+ *
+ * 向数组中的每个整数前添加 '+' 或 '-' ，然后串联起所有整数，可以构造一个 表达式 ：
+ *
+ * 例如，nums = [2, 1] ，可以在 2 之前添加 '+' ，在 1 之前添加 '-' ，然后串联起来得到表达式 "+2-1" 。
+ * 返回可以通过上述方法构造的、运算结果等于 target 的不同 表达式 的数目。
+ *
+ * @author Shenhan
+ * @create 2021-09-09-18:04
+ */
+public class Solution_494 {
+
+    public int findTargetSumWays(int[] nums, int target) {
+        int sum = 0;
+        for (int num : nums) {
+            sum += num;
+        }
+        int diff = sum - target;
+        if (diff < 0 || diff % 2 != 0) {
+            return 0;
+        }
+        int neg = diff / 2;
+        int[] dp = new int[neg + 1];
+        dp[0] = 1;
+        for (int num : nums) {
+            for (int j = neg; j >= num; j--) {
+                dp[j] += dp[j - num];
+            }
+        }
+        return dp[neg];
+    }
+}
